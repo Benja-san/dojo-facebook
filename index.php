e4ffbcdd448509a3aae2d941158913f8d795ea37
Joël MAYEMBA <joelmayemba@gmail.com>
2023-12-19 10:42:46 +0100

    Hajodi branch
    La branche de Hazar, Joël et DinhHuy

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
-// `$users` contenant une série de prénoms (Simon, Arthur, Yohan),
-// * *"Soyez le premier à aimer cette publication"*,
+
+
 // * *"Simon aime cette publication"*,
 // * *"Simon et Arthur aiment cette publication"*,
 // * *"Simon et X autres personnes ont aimé cette publication."*,
 
 /////////////////////////zone d'attente
 // uncomment lines for testing
-$users = [];
+
+
+
+
 // $users = ['Simon'];
 // $users = ['Yohan'];
 // $users = ['Pierre', 'Marie'];
@@ -17,3 +20,33 @@ $users = [];
 // $users = ['Virginie', 'Paul', 'Julien', 'Simon'];
 
 //write you're code below
+
+// `$users` contenant une série de prénoms (Simon, Arthur, Yohan),
+$users = ["Simon","Yohan","Arthur","Paul","Pierre","Marie","Virginie","Valentin","Laurent","Julien"];
+print_r($users);
+
+// Randomly select a nunmber of first name for testing
+
+$userCount = rand(0, 4);
+
+// * *"Soyez le premier à aimer cette publication"*,
+$like = 1;
+$post = 0+$like;
+
+echo $post;
+
+foreach ($users as $haveLiked => $post) {
+    echo $haveLiked . " - " . $post . PHP_EOL;
+}
+
+//#Retourne le nombre de personnes qui ont aimés la publication
+echo "Il y'a ". count($users). " " . "qui ont aimés la publication.";
+
+
+// Function gernerate Sentence
+
+function generateSentence($userCount, $users) {
+    switch ($userCount) {
+        
+    }
+}
