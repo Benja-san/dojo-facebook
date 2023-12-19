dbfe9d408346b38453efd7249c0f855b2c7d6a64
Joël MAYEMBA <joelmayemba@gmail.com>
2023-12-19 10:58:41 +0100

    Update

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ print_r($users);
 
 // Randomly select a nunmber of first name for testing
 
-$userCount = rand(0, 4);
+$userCount = rand(0, 9);
 
 // * *"Soyez le premier à aimer cette publication"*,
 $like = 1;
@@ -42,11 +42,26 @@ foreach ($users as $haveLiked => $post) {
 //#Retourne le nombre de personnes qui ont aimés la publication
 echo "Il y'a ". count($users). " " . "qui ont aimés la publication.";
 
+// Get random name
+
+function getRandomName($users) {
+    $randomIndex = array_rand($users);
+    return $users[$randomIndex];
+}
 
 // Function gernerate Sentence
 
 function generateSentence($userCount, $users) {
     switch ($userCount) {
-        
+        case 0:
+            return "Be the first to like this post";
+        case 1:
+            return getRandomName($users) . " like this post";
+        case 2:
+            return implode(" and ", array_slice($users, 0, 2)) . " like this post";
+        default:
+            return current($users) . " and " . (count($users) - 1) . " other people liked this post";
     }
 }
+
+echo generateSentence($userCount, $users);
